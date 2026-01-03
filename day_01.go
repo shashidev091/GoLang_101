0ca4c196bf68888d14f45fc7ea248766c1545b89
Shashi Bhagat <skujur871@gmail.com>
2026-01-04 01:00:35 +0530

    learned printf and Sprintf along with parsing and taking inputs from users

--- day_01.go
+++ day_01.go
@@ -1,7 +1,7 @@
-package main
+package main // Used to group the files similar to java
 
 import (
-	"fmt"
+	"fmt" // core package, knows as format
 	"math"
 )
 
@@ -20,4 +20,52 @@ func main() {
 
 	var futureRealValue float64 = futureValue / math.Pow(1+inflationRate/100, float64(years))
 	fmt.Println(futureRealValue)
+
+	// Check your date of birth (Using only single words/digits with Scan)
+	age := calculate_age()
+
+	//Create a profit calulator
+	// profitCalculator()
+
+	var introduction = fmt.Sprintf("Hello, My name is Shashi Bhagat and I am %d years old. I am learning Go Language.", age)
+	fmt.Println(introduction)
+}
+
+func calculate_age() int {
+	const currentYear int = 2025
+	var yearOfBirth uint
+	var age int
+
+	fmt.Print("Enter your year of birth: ")
+	fmt.Scan(&yearOfBirth)
+	age = currentYear - int(yearOfBirth)
+	fmt.Printf("%T\n", currentYear)
+	fmt.Println("You are", age, "Years old.")
+	return age
+}
+
+func profitCalculator() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Please enter the revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Please enter expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Please enter the Tax rate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+
+	ratio := ebt / profit
+
+	// fmt.Println(ebt)
+	// fmt.Println(profit)
+	// fmt.Println(ratio)
+
+	fmt.Printf("Your ebt is: %v.\nYour profit is: %v.\nYour ratio is : %.2f.", ebt, profit, ratio)
 }
