28321efd98b950667306b8dada399add38ef3acd
Shashi Bhagat <skujur871@gmail.com>
2026-01-04 21:43:41 +0530

    switch base advance with file io

--- day_02.go
+++ day_02.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func DayTwo() {
 	fmt.Println("This is Day 2 of Go Bootcamp!")
@@ -70,6 +73,11 @@ func drawRightAngleTriangleReverse(number int) {
 	}
 }
 
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile("balance.txt", []byte(balanceText), 0644)
+}
+
 func switchCase(choice int) {
 	switch choice {
 	case 1:
@@ -85,6 +93,59 @@ func switchCase(choice int) {
 	case 3:
 		drawRightAngleTriangleReverse(5)
 		fmt.Println("================END==================")
+	case 4:
+		accountBalance := 10000.0
+		for {
+			fmt.Println("What do you want to do?")
+			fmt.Println("1. Check balance")
+			fmt.Println("2. Deposit money")
+			fmt.Println("3. Withdraw money")
+			fmt.Println("4. Exit")
+
+			var innerChoice int
+			fmt.Print("Your choice: ")
+			fmt.Scan(&innerChoice)
+
+			switch innerChoice {
+			case 1:
+				fmt.Println("Your balance is", accountBalance)
+			case 2:
+				fmt.Println("Your deposit: ")
+				var depositAmount float64
+				fmt.Scan(&depositAmount)
+
+				if depositAmount <= 0 {
+					fmt.Println("Invalid amount. Must be greater than 0.")
+					continue
+				}
+
+				accountBalance += depositAmount
+				fmt.Println("Balance updated! New amount: ", accountBalance)
+				writeBalanceToFile(accountBalance)
+			case 3:
+				fmt.Print("Withdraw amount: ")
+				var withdrawalAmount float64
+				fmt.Scan(&withdrawalAmount)
+
+				if withdrawalAmount <= 0 {
+					fmt.Println("Invalid amount. Must be greater than 0.")
+					continue
+				}
+
+				if withdrawalAmount > accountBalance {
+					fmt.Println("Invalid amount. You can't withdraw more than you have.")
+					continue
+				}
+
+				accountBalance -= withdrawalAmount
+				fmt.Println("Balance updated! New amount: ", accountBalance)
+				writeBalanceToFile(accountBalance)
+			default:
+				fmt.Println("Goodbye!")
+				fmt.Println("Thanks for banking with us.")
+				return
+			}
+		}
 	default:
 		fmt.Println("Wrong Input")
 	}
